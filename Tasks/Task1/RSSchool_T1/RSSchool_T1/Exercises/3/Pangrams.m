886c56f4eedf089aa9bfcd9c6ee232b43222d7f3
Artyom Zhytsko <tyoma@MB-Tyoma.local>
2019-03-24 01:38:48 +0300

    feat: add pangrams

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
@@ -4,7 +4,15 @@
 
 // Complete the pangrams function below.
 - (BOOL)pangrams:(NSString *)string {
-    return NO;
+    const int alphabetLength = 26;
+    NSString *lowercaseStringWithoutWhitespaces = [string stringByReplacingOccurrencesOfString:@" "  withString:@""].lowercaseString;
+    unichar letter = 'a';
+    for (int i = 0; i < alphabetLength; i++, letter++) {
+        if (![lowercaseStringWithoutWhitespaces containsString:[NSString stringWithFormat:@"%c", letter]]){
+            return NO;
+        }
+    }
+    return YES;
 }
 
 @end
