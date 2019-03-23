785667a68315697e63956b5317391677c97ecaa1
Artyom Zhytsko <tyoma@MB-Tyoma.local>
2019-03-23 23:58:31 +0300

    feat: add diagonalDifference

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
@@ -4,7 +4,17 @@
 
 // Complete the diagonalDifference function below.
 - (NSNumber *) diagonalDifference:(NSArray *)array {
-    return 0;
+    int sum = 0;
+    NSUInteger matrixSize = [array count];
+    NSMutableArray *matrix = [NSMutableArray arrayWithCapacity: matrixSize];
+    for (NSString *row in array){
+        [matrix addObject: [row componentsSeparatedByString:@" "]];
+    }
+    for (int i = 0; i < matrixSize; i++){
+        sum += [(NSNumber *)matrix[i][i] intValue];
+        sum -= [(NSNumber *)matrix[matrixSize - i - 1][i] intValue];
+    }
+    return sum > 0 ? @(sum) : @(-sum);
 }
 
 @end
