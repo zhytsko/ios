e0cff8d34bc47ed287f52851ea638cd2bcfe17ad
Artyom Zhytsko <tyoma@MB-Tyoma.local>
2019-03-25 00:58:17 +0300

    feat: add encryption

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
@@ -4,7 +4,24 @@
 
 // Complete the encryption function below.
 - (NSString *)encryption:(NSString *)string {
-    return @"";
+    NSString *stringWithoutWhitespaces = [string stringByReplacingOccurrencesOfString:@" "  withString:@""];
+    double stringLength = [stringWithoutWhitespaces length];
+    int rows = floor(sqrt(stringLength));
+    int columns = ceil(sqrt(stringLength));
+    if (rows * columns < stringLength) {
+        rows < columns ? rows++ : columns++;
+    }
+    NSMutableString *result = [NSMutableString stringWithString:@""];
+    for (int i = 0; i < columns; i++) {
+        for (int j = i; j < stringLength; j+=columns) {
+            NSString *letter = [NSString stringWithFormat:@"%c",[stringWithoutWhitespaces characterAtIndex: j]];
+            [result appendString: letter];
+        }
+        if (i < columns - 1) {
+            [result appendString:@" "];
+        }
+    }
+    return result;
 }
 
 @end
