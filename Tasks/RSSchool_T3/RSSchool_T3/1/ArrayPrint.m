2ba8b4a75ca7a7ffb4eeb6d1daea5807105b72f9
Artyom <zhytsko.a@gmail.com>
2019-04-11 16:48:09 +0300

    feat: implement print

--- Tasks/RSSchool_T3/RSSchool_T3/1/ArrayPrint.m
+++ Tasks/RSSchool_T3/RSSchool_T3/1/ArrayPrint.m
@@ -1,1 +1,61 @@
 #import "ArrayPrint.h"
+
+int check(id object) {
+    if ([object isKindOfClass:[NSNumber class]]){
+        return 0;
+    } else if ([object isKindOfClass:[NSNull class]]) {
+        return 1;
+    } else if ([object isKindOfClass:[NSArray class]]) {
+        return 2;
+    } else if ([object isKindOfClass:[NSString class]]) {
+        return 3;
+    }
+    return -1;
+}
+
+NSString *objToString(id object) {
+    NSMutableString *result = [[NSMutableString alloc] init];
+    switch (check(object)) {
+        case 0:
+            [result appendFormat:@"%@,", object];
+            break;
+        case 1:
+            [result appendString: @"null,"];
+            break;
+        case 2:
+            [result appendString:@"["];
+            for (id obj in object) {
+                [result appendString: objToString(obj)];
+            }
+            [result appendString:@"],"];
+            break;
+        case 3:
+            [result appendFormat:@"\"%@\",", object];
+            break;
+        default:
+            [result appendString:@"unsupported,"];
+            break;
+    }
+    [result autorelease];
+    return result;
+}
+
+@implementation NSArray (RSSchool_Extension_Name)
+- (NSString *)print{
+    NSMutableString *result = [[NSMutableString alloc] init];
+    [result appendString:@"["];
+    NSArray *array = self;
+    for (int i = 0; i < [array count]; i++) {
+        [result appendString: objToString(array[i])];
+    }
+    [result appendString:@"]"];
+    [result replaceOccurrencesOfString:@",]" withString:@"]" options:0 range: NSMakeRange(0, [result length])];
+    [result autorelease];
+    return result;
+}
+
+@end
+
+
+
+
--- Tasks/RSSchool_T3/RSSchool_T3/2/FullBinaryTrees.m
+++ Tasks/RSSchool_T3/RSSchool_T3/2/FullBinaryTrees.m
@@ -1,2 +1,13 @@
 #import "FullBinaryTrees.h"
-// good luck
+@implementation FullBinaryTrees
+- (NSString *)stringForNodeCount:(NSInteger)count{
+    if ((count - 1) % 2 != 0) {
+        return @"[]";
+    }
+//    if (count == 3) {
+//        return
+//    }
+    return nil;
+}
+@end
+
